510337417dc30113a1e90b3e3fe24911cffa25c9
Muhammad Bilal Qamar <42898923+MuhammadBilalQamar@users.noreply.github.com>
2019-09-17 21:59:09 +0500

    Update Firebase.js

--- Database/Firebase.js
+++ Database/Firebase.js
@@ -1,12 +1,7 @@
 import * as fire from "firebase";
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDJ4KP_1ORRx7vf-a5hMWL6_RZp7m4pMyw",
-  authDomain: "saylani-final-hackathon.firebaseapp.com",
-  databaseURL: "https://saylani-final-hackathon.firebaseio.com",
-  projectId: "saylani-final-hackathon",
-  storageBucket: "saylani-final-hackathon.appspot.com",
-  messagingSenderId: "283965223324"
+//ENTER YOUR SCRIPT HERE
 };
 
 fire.initializeApp(config);
